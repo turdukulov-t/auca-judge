2cf4827ee98fa418f8214fd8fe3da41ff3780f62
Behruz Amonbekov <bamonbekov1@gmail.com>
2024-03-12 01:32:47 +0600

    AJ-79: Create Entities for Problem, Submission, Difficulty and Category

---
+++ AUCA.Domain/Entity/Difficulty.cs
@@ -0,0 +1,7 @@
+﻿namespace AUCA.Domain.Entity;
+public class Difficulty
+{
+	public int Id { get; set; }
+
+	public string Name { get; set; }
+}
---
+++ AUCA.Domain/Entity/Problem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AUCA.Domain.Entity;
+public class Problem
+{
+	public string Id { get; set; }
+
+	[MaxLength(256)]
+	public string Name { get; set; }
+
+	[MaxLength(Int32.MaxValue)]
+	public string Description { get; set; }
+
+	public int DifficultyId { get; set; }
+
+	public Difficulty Difficulty { get; set; }
+
+	public int CategoryId { get; set; }
+
+	public Category Category { get; set; }
+
+	public ICollection<Submission> Submissions { get; set; } = new List<Submission>();
+}
